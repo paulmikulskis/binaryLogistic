cce6d297e91a9a5588259f91f46970e01167bbcc
Paul Mikulskis <mikulskisp@gmail.com>
2018-05-21 14:25:08 +0000

    basic untuned model working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from six.moves import cPickle as pickle
 from  PIL import Image
 from random import randint
+from network import Network
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -16,11 +17,11 @@ labels = tupled_data['labels']
 #rezip images and labels into list of tuples [(image, label)]
 pairs = zip(img, labels)
 #filter out only pictures with label cat (label = 3)
-tupledTwo = list(filter(lambda x: x[1] == 3, pairs))
-#actual image data of all cats
-cats_img = [t[0] for t in tupledTwo]
+tupledTwoCats = list(filter(lambda x: x[1] == 3, pairs))
+#actual image data of all cats, reshaped into MxNx3
+cats_img = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoCats]
 #prints the number of cats we have
-print(len(cats_img))
+print('cats:',len(cats_img))
 
 #supplementary section of code for opening up the label-string list
 f = open('/home/paul/python/project_1_ind/binaryLogistic/cifar-10-batches-py/batches.meta', 'rb')
@@ -28,11 +29,75 @@ tupled_data= pickle.load(f, encoding='latin1')
 f.close()
 label_names = tupled_data['label_names']
 #prints all the labels in the dataset as a whole
-list(map(lambda x: print(x), label_names))
+#list(map(lambda x: print(x), label_names))
 
-#displays a random cat from the dataset
-single_img = np.array(cats_img[randint(0,len(cats_img))])
-#reshapes the image data into something usable
-single_img_reshaped = np.transpose(np.reshape(single_img,(3, 32,32)), (1,2,0))
-plt.imshow(single_img_reshaped)
-plt.show()
+
+#collect nxMxMx3 set of images of cat vs non-cat, and their corresponding labels
+#in a 1xn matrix, with 1 for cat and 0 for non-cat image
+tupledTwoTruck = list(filter(lambda x: x[1] == 9, zip(img, labels)))
+trucks_img = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoTruck]
+print('trucks:', len(trucks_img))
+
+
+#displays a random truck from the dataset
+single_img = np.array(trucks_img[randint(0,len(trucks_img))])
+plt.imshow(single_img)
+#plt.show()
+
+#sets up the array structures for training
+X = np.zeros((single_img.shape[0]*single_img.shape[1]*3, 600))
+Y = np.zeros((1,600))
+for i in range(0,600):
+    d = randint(0,1)
+    if d == 1:
+        X[:,i] = np.reshape(cats_img[i], (single_img.shape[0]*single_img.shape[1]*3))
+        Y[0,i] = 1
+    else:
+        X[:,i] = np.reshape(trucks_img[i], (single_img.shape[0]*single_img.shape[1]*3))
+        Y[0,i] = 0
+#checking sizes
+print('using 600 training examples...')
+print('shape of X input:', X.shape)
+print('shape of Y input:', Y.shape)
+print('number of 1s:', np.where(Y == 1)[0].shape[0])
+print('number of 1s:', np.where(Y == 0)[0].shape[0])
+
+
+#train on the network:
+network = Network(single_img.shape[0]*single_img.shape[1]*3,0.2)
+network.train(X,Y,100)
+
+#test the network:
+#first we must grab the training sets of images:
+f = open('/home/paul/python/project_1_ind/binaryLogistic/cifar-10-batches-py/data_batch_2', 'rb')
+tupled_data= pickle.load(f, encoding='latin1')
+f.close()
+#seperate the image and label data
+img2 = tupled_data["data"]
+labels2 = tupled_data['labels']
+#rezip images and labels into list of tuples [(image, label)]
+pairs = zip(img2, labels2)
+#filter out only pictures with label cat (label = 3)
+tupledTwoCats2 = list(filter(lambda x: x[1] == 3, pairs))
+#actual image data of all cats, reshaped into MxNx3
+cats_img2 = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoCats2]
+
+#collect nxMxMx3 set of images of cat vs non-cat, and their corresponding labels
+#in a 1xn matrix, with 1 for cat and 0 for non-cat image
+tupledTwoTruck2 = list(filter(lambda x: x[1] == 9, zip(img2, labels2)))
+trucks_img2 = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoTruck2]
+
+
+X = np.zeros((single_img.shape[0]*single_img.shape[1]*3, 600))
+Y = np.zeros((1,600))
+for i in range(0,600):
+    d = randint(0,1)
+    if d == 1:
+        X[:,i] = np.reshape(cats_img2[i], (single_img.shape[0]*single_img.shape[1]*3))
+        Y[0,i] = 1
+    else:
+        X[:,i] = np.reshape(trucks_img2[i], (single_img.shape[0]*single_img.shape[1]*3))
+        Y[0,i] = 0
+
+accuracy = network.test(X,Y)
+print('accuracy of the network = ', accuracy)
