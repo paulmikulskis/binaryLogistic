3bfc78ffca62d5440ab5943cab053b434c28c22c
Paul Mikulskis <mikulskisp@gmail.com>
2018-05-21 18:17:13 +0000

    two datasets working

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
+
 # binaryLogistic
+
+## A Simple Binary Logitic Classification Model on Two Datasets
+
+
+#### Goal
+---
+The goal of this project was to get myself familiar with binary logistic regression.
+To do this, I tried identifying pictures of cats, among non-cat images.  The data would be
+classified as 1 if it were a cat, and 0 otherwise.
+
+#### Project Layout
+---
+This project has three important files:
+* main.py
+* main2.py
+* network.py
+
+Each of the 'main' files works with a different dataset.  The first one works with a pre-procured cat vs. non-cat dataset in the format of an h5 file that I obtained from Andrew Ng's deep learning AI course.  The second one works with the CIFAR-10 dataset, and has some code to procure and sort through the data before running the model.  
+
+The network.py file contains the object representing my small neural net.  This file does the processing of data given from any dataset as long as it is in the correct format.  
+#### How To Run
+---
+To run the model, just run either of the main files with
+
+    python main.py
+or
+
+    python main2.py
+
+ And the accuracy of the model's prediction will output to console along with the logistic cost function evaluation every 100 epochs.  
+
+For customizing different hyper-parameters, constants provided to the Network object can be modified.  When instantiating the Network object, the second agrument is alpha, the learning rate of the network:
+`network = Network(X.shape[0],0.0005)`, and when the train() method is called, the third parameter is the number of epochs: `network.train(X,Y, 2000)`.
+
+#### Results, Observations and Additional Notes
+---
+While playing with this project, I noticed some interesting behaviour that I wanted to note:
+1) The dataset I used from Andrew Ng's course contained pictures of cats along with other images of various types.  I was able to aget the accuracy up to 70% using a learning rate of 0.005 and 2,000 epochs.  However, if I changed the learning rate to 0.05 and ran 200 epochs, I noticed an accuracy of 80%.  This led me to believe that though I thought more epochs is good, the data might have been getting overfitted at 2,000 epochs.  
+
+2) When using the CIFAR-10 dataset, the main2.py file procures two sets of data, one of cats, and one of trucks, then chooses 600 images from the union of the two sets.  Using a distribution that had two distinct classes of data lef me to be able to boast accuracy of up to 81% using a learning rate of 0.005 and 2,000 epochs.  I reckon this is because in this training exercise, there were exactly two sets of classifications, and there was probably a better classification of what was and what was not a cat.
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from six.moves import cPickle as pickle
 from  PIL import Image
 from random import randint
@@ -6,98 +5,44 @@ from network import Network
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import h5py
 
-#open file from CIFAR10
-f = open('/home/paul/python/project_1_ind/binaryLogistic/cifar-10-batches-py/data_batch_1', 'rb')
-tupled_data= pickle.load(f, encoding='latin1')
-f.close()
-#seperate the image and label data
-img = tupled_data["data"]
-labels = tupled_data['labels']
-#rezip images and labels into list of tuples [(image, label)]
-pairs = zip(img, labels)
-#filter out only pictures with label cat (label = 3)
-tupledTwoCats = list(filter(lambda x: x[1] == 3, pairs))
-#actual image data of all cats, reshaped into MxNx3
-cats_img = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoCats]
-#prints the number of cats we have
-print('cats:',len(cats_img))
+#This loads the H5 dataset from Andrew Ng's deep learning course dataset of cat vs. non cat images
+def load_dataset():
+    train_dataset = h5py.File('catvnoncat/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    test_dataset = h5py.File('catvnoncat/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set label
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
 
-#supplementary section of code for opening up the label-string list
-f = open('/home/paul/python/project_1_ind/binaryLogistic/cifar-10-batches-py/batches.meta', 'rb')
-tupled_data= pickle.load(f, encoding='latin1')
-f.close()
-label_names = tupled_data['label_names']
-#prints all the labels in the dataset as a whole
-#list(map(lambda x: print(x), label_names))
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-
-#collect nxMxMx3 set of images of cat vs non-cat, and their corresponding labels
-#in a 1xn matrix, with 1 for cat and 0 for non-cat image
-tupledTwoTruck = list(filter(lambda x: x[1] == 9, zip(img, labels)))
-trucks_img = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoTruck]
-print('trucks:', len(trucks_img))
-
-
-#displays a random truck from the dataset
-single_img = np.array(trucks_img[randint(0,len(trucks_img))])
-plt.imshow(single_img)
-#plt.show()
+#load the data
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+n_train = train_set_x_orig.shape[0]
+n_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
 
 #sets up the array structures for training
-X = np.zeros((single_img.shape[0]*single_img.shape[1]*3, 600))
-Y = np.zeros((1,600))
-for i in range(0,600):
-    d = randint(0,1)
-    if d == 1:
-        X[:,i] = np.reshape(cats_img[i], (single_img.shape[0]*single_img.shape[1]*3))
-        Y[0,i] = 1
-    else:
-        X[:,i] = np.reshape(trucks_img[i], (single_img.shape[0]*single_img.shape[1]*3))
-        Y[0,i] = 0
-#checking sizes
-print('using 600 training examples...')
-print('shape of X input:', X.shape)
-print('shape of Y input:', Y.shape)
-print('number of 1s:', np.where(Y == 1)[0].shape[0])
-print('number of 1s:', np.where(Y == 0)[0].shape[0])
-
+X = train_set_x
+Y = train_set_y
 
 #train on the network:
-network = Network(single_img.shape[0]*single_img.shape[1]*3,0.2)
-network.train(X,Y,100)
+network = Network(X.shape[0],0.005)
+network.train(X,Y, 2000)
 
-#test the network:
-#first we must grab the training sets of images:
-f = open('/home/paul/python/project_1_ind/binaryLogistic/cifar-10-batches-py/data_batch_2', 'rb')
-tupled_data= pickle.load(f, encoding='latin1')
-f.close()
-#seperate the image and label data
-img2 = tupled_data["data"]
-labels2 = tupled_data['labels']
-#rezip images and labels into list of tuples [(image, label)]
-pairs = zip(img2, labels2)
-#filter out only pictures with label cat (label = 3)
-tupledTwoCats2 = list(filter(lambda x: x[1] == 3, pairs))
-#actual image data of all cats, reshaped into MxNx3
-cats_img2 = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoCats2]
+#arrange testing data
+X = test_set_x
+Y = test_set_y
 
-#collect nxMxMx3 set of images of cat vs non-cat, and their corresponding labels
-#in a 1xn matrix, with 1 for cat and 0 for non-cat image
-tupledTwoTruck2 = list(filter(lambda x: x[1] == 9, zip(img2, labels2)))
-trucks_img2 = [np.transpose(np.reshape(t[0],(3,32,32)), (1,2,0)) for t in tupledTwoTruck2]
-
-
-X = np.zeros((single_img.shape[0]*single_img.shape[1]*3, 600))
-Y = np.zeros((1,600))
-for i in range(0,600):
-    d = randint(0,1)
-    if d == 1:
-        X[:,i] = np.reshape(cats_img2[i], (single_img.shape[0]*single_img.shape[1]*3))
-        Y[0,i] = 1
-    else:
-        X[:,i] = np.reshape(trucks_img2[i], (single_img.shape[0]*single_img.shape[1]*3))
-        Y[0,i] = 0
-
+#test the network
 accuracy = network.test(X,Y)
 print('accuracy of the network = ', accuracy)
